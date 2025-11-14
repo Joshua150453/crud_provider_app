0afe346b5a37ac7e4f98ab122e5464240d042922
Joshua150453 <jortizr@ulasalle.edu.pe>
2025-11-13 19:50:39 -0500

    Ultima modificacion hecha en main dart, para cargar correctamente
    Added comments for better code documentation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
+/*
+ * Actividad principal (Formulario)
+ * Autor: Joshua David Ortiz Rosas
+ * Fecha creación: 30/10/2025
+ * Última modificación: 02/11/2025
+ */
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'viewmodels/user_view_model.dart';
 import 'views/user_list_screen.dart';
 
 void main() {
+  // Se inicia la app usando Provider para manejar el estado global
   runApp(
     ChangeNotifierProvider(
-      create: (_) => UserViewModel(),
-      child: const MyApp(),
+      create: (_) => UserViewModel(), // Crea el ViewModel de usuarios
+      child: const MyApp(),           // Widget raíz de la aplicación
     ),
   );
 }
@@ -19,8 +27,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Gestión de Usuarios',
-      theme: ThemeData(primarySwatch: Colors.indigo),
-      home: const UserListScreen(),
+      theme: ThemeData(primarySwatch: Colors.indigo), // Tema principal
+      home: const UserListScreen(), // Pantalla inicial de la app
     );
   }
 }
