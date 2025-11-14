cfdfe006ea2e4b9c15db315de0d159c314fb1fc5
Joshua150453 <jortizr@ulasalle.edu.pe>
2025-11-13 19:43:17 -0500

    Agregando datos para correo al user.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,15 +1,16 @@
+// Modelo que representa a un usuario del sistema
 class User {
   String nombre;
   String genero; // 'Masculino' o 'Femenino'
-  int edad;
-  String correo;
   bool activo;
+  int edad; // Nueva propiedad edad
+  String correo; // Nueva propiedad correo electrónico
 
   User({
     required this.nombre,
     required this.genero,
+    required this.activo,
     required this.edad,
     required this.correo,
-    required this.activo,
   });
 }
